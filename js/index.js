80f44eff285b53521701437309d224c3faef3fbf
pedro <pbidoggia@digitalhouse.com>
2019-11-23 20:15:12 -0300

    img

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 function myfunction() {
-    document.querySelector('body').innerHTML = `<h1> This is an experiment </1>`;
+    document.querySelector('body').innerHTML = `
+    <h1> This is an experiment </h1>
+    <img src='img/img.gif' alt='image'>
+    `;
 }
 window.addEventListener('load', myfunction());
